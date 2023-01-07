a44192b3888dcbba1d380ac008fcfd6792a8e4ca
mulasau <mikevdga@gmail.com>
2023-01-07 17:33:28 -0500

    1.7 17:32 Added readme.

---
+++ src/main/java/com/javarush/domain/CountryLanguage.java
@@ -0,0 +1,69 @@
+package com.javarush.domain;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.Type;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(schema = "world", name = "country_language")
+public class CountryLanguage {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "country_id", referencedColumnName = "id")
+    private Country country;
+
+    @Column(name = "language")
+    private String language;
+
+    @Column(name = "is_official", columnDefinition = "BIT")
+    @Type(type = "org.hibernate.type.BooleanType")
+    private Boolean isOfficial;
+
+    @Column(name = "percentage")
+    private BigDecimal percentage;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Country getCountry() {
+        return country;
+    }
+
+    public void setCountry(Country country) {
+        this.country = country;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public Boolean getOfficial() {
+        return isOfficial;
+    }
+
+    public void setOfficial(Boolean official) {
+        isOfficial = official;
+    }
+
+    public BigDecimal getPercentage() {
+        return percentage;
+    }
+
+    public void setPercentage(BigDecimal percentage) {
+        this.percentage = percentage;
+    }
+}
